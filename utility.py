568c6158214096005366f63a72e40cf0daa7fc90
leo19900723 <leo19900723@gmail.com>
2020-05-31 00:43:03 -0700

    [add] output file utility

---
+++ utility.py
@@ -0,0 +1,16 @@
+
+def main():
+    fileName = "Trial1"
+    with open("Output/" + fileName, "r", encoding="utf-8") as inputFile:
+        data = inputFile.read().split("\n")
+
+    with open("Output/Losses_Rewards_" + fileName + ".csv", "w", encoding="utf-8") as outputFile:
+        outputFile.write("losses, rewards\n")
+        for index in range(len(data)):
+            outputFile.write(data[index].split(" ")[-1] + ("\n" if index % 2 else ", "))
+        outputFile.close()
+    return
+
+
+if __name__ == '__main__':
+    main()
